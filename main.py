50554a10eecc6663f7edd0554394513e455c7e36
Armando233-abc <armandovigliotti5@gmail.com>
2022-03-09 22:49:28 +0100

    create snake and basic move

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-# Simple pygame program
-
-# Import and initialize the pygame library
 import pygame
 pygame.init()
 
-# Set up the drawing window
 screen = pygame.display.set_mode([800, 600])
+screen.fill((0, 255, 0))
 
-# Run until the user asks to quit
+snake = pygame.image.load("square.png")
+snake = pygame.transform.scale(snake, (50, 50))
+snake_X_change = 0
+snake_Y_change = 0
+
+def left_right(x, y):
+    screen.blit(snake, (x, y))
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                snake_X_change -= 50
 
-    # Fill the background with white
-    screen.fill((0, 255, 0))
+            if event.key == pygame.K_RIGHT:
+                snake_X_change += 50
 
-    pygame.draw.rect(screen, 'red', (300, 200, 50, 50))
+            if event.key == pygame.K_UP:
+                snake_Y_change -= 50
 
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 20)
-    # Flip the display
-    pygame.display.flip()
+            if event.key == pygame.K_DOWN:
+                snake_Y_change += 50
 
-# Done! Time to quit.
+    left_right(snake_X_change, snake_Y_change)
+    pygame.display.update()
+
+
+
 pygame.quit()
