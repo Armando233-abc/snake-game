50549d534a396984551538bf6ba8df4082c95681
Armando233-abc <armandovigliotti5@gmail.com>
2022-03-10 19:35:15 +0100

    better snake move

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode([800, 800])
 
 snake = pygame.image.load("square.png")
 snake = pygame.transform.scale(snake, (50, 50))
+snake_X = 0
+snake_Y = 0
 snake_X_change = 0
 snake_Y_change = 0
 
@@ -22,29 +24,38 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                snake_X_change -= 50
+                snake_X_change = -0.5
 
-            if event.key == pygame.K_RIGHT:
-                snake_X_change += 50
+            elif event.key == pygame.K_RIGHT:
+                snake_X_change = 0.5
 
-            if event.key == pygame.K_UP:
-                snake_Y_change -= 50
+            elif event.key == pygame.K_UP:
+                snake_Y_change = -0.5
 
-            if event.key == pygame.K_DOWN:
-                snake_Y_change += 50
+            elif event.key == pygame.K_DOWN:
+                snake_Y_change = 0.5
 
-    if snake_X_change <= 0:
-        snake_X_change = 750
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                snake_X_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                snake_Y_change = 0
 
-    elif snake_X_change >= 800:
-        snake_X_change = 0.1
+    snake_X += snake_X_change
+    snake_Y += snake_Y_change
 
-    if snake_Y_change <= 0:
-        snake_Y_change = 750
+    if snake_X < 0:
+        snake_X = 750
 
-    elif snake_Y_change >= 800:
-        snake_Y_change = 0.1
+    elif snake_X >= 800:
+        snake_X = 0.1
 
-    left_right(snake_X_change, snake_Y_change)
+    if snake_Y <= 0:
+        snake_Y = 750
+
+    elif snake_Y >= 800:
+        snake_Y = 0.1
+
+    left_right(snake_X, snake_Y)
     pygame.display.update()
 
